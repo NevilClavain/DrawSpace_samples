3d8e4f87485051cf722106c59884e5c1a0773a24
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2013-09-28 10:50:48 +0200

    - updating samples projects directories - samples sources : replacing tabs by spaces

--- fog/dsappclient.h
+++ fog/dsappclient.h
@@ -8,24 +8,24 @@
 class SceneTransform
 {
 protected:
-	dsreal									m_xangle, m_yangle;
-	dsreal									m_zpos;
-	DrawSpace::Utils::Mutex					m_mutex_angles;
-	DrawSpace::Utils::Mutex					m_mutex_z;
+    dsreal                                  m_xangle, m_yangle;
+    dsreal                                  m_zpos;
+    DrawSpace::Utils::Mutex                 m_mutex_angles;
+    DrawSpace::Utils::Mutex                 m_mutex_z;
 
-	DrawSpace::Utils::Mutex					m_mutex_result;
-	DrawSpace::Utils::Matrix				m_result;
+    DrawSpace::Utils::Mutex                 m_mutex_result;
+    DrawSpace::Utils::Matrix                m_result;
 
 public:
-	SceneTransform( void );
-	~SceneTransform( void );
+    SceneTransform( void );
+    ~SceneTransform( void );
 
-	void Run( void );
-	void Transform( void );
-	void GetResult( DrawSpace::Utils::Matrix& p_out );
+    void Run( void );
+    void Transform( void );
+    void GetResult( DrawSpace::Utils::Matrix& p_out );
 
-	void OnLeftDrag( DrawSpace::Utils::TimeManager& p_timer, long p_dx, long p_dy );
-	void OnRightDrag( DrawSpace::Utils::TimeManager& p_timer, long p_dx, long p_dy );
+    void OnLeftDrag( DrawSpace::Utils::TimeManager& p_timer, long p_dx, long p_dy );
+    void OnRightDrag( DrawSpace::Utils::TimeManager& p_timer, long p_dx, long p_dy );
 };
 
 
@@ -35,30 +35,30 @@ class dsAppClient : public DrawSpace::App
 {
 protected:
 
-    static dsAppClient*							m_instance;
+    static dsAppClient*                         m_instance;
 
 
     DrawSpace::Scenegraph                       m_scenegraph;
     DrawSpace::ChunkNode*                       m_chunknode;
 
 
-	DrawSpace::FinalPass*						m_finalpass;
-	DrawSpace::IntermediatePass*				m_fogblendpass;
+    DrawSpace::FinalPass*                       m_finalpass;
+    DrawSpace::IntermediatePass*                m_fogblendpass;
 
-    DrawSpace::IntermediatePass*				m_fogintpass;
-    DrawSpace::IntermediatePass*				m_texturepass;
+    DrawSpace::IntermediatePass*                m_fogintpass;
+    DrawSpace::IntermediatePass*                m_texturepass;
 
 
-	SceneTransform*								m_transform;
-	DrawSpace::Core::Task<SceneTransform>*		m_transformtask;
-	DrawSpace::Utils::TimeManager				m_timer;
+    SceneTransform*                             m_transform;
+    DrawSpace::Core::Task<SceneTransform>*      m_transformtask;
+    DrawSpace::Utils::TimeManager               m_timer;
 
-	bool										m_mouselb;
-	bool										m_mouserb;
+    bool                                        m_mouselb;
+    bool                                        m_mouserb;
 
-	DrawSpace::Gui::TextWidget*					m_fpstext_widget;
+    DrawSpace::Gui::TextWidget*                 m_fpstext_widget;
 
-	DrawSpace::Core::Font*						m_font;
+    DrawSpace::Core::Font*                      m_font;
 
 
     dsAppClient( void );
@@ -78,19 +78,19 @@ public:
         return m_instance;
     }
 
-	virtual void		OnRenderFrame( void );
-	virtual bool		OnIdleAppInit( void );
-	virtual void		OnAppInit( void );
-	virtual void		OnClose( void );
-	virtual void        OnKeyPress( long p_key ) ;
-	virtual void        OnEndKeyPress( long p_key );
-	virtual void        OnKeyPulse( long p_key );
-	virtual void		OnMouseMove( long p_xm, long p_ym, long p_dx, long p_dy );
-	virtual void		OnMouseLeftButtonDown( long p_xm, long p_ym );
-	virtual void		OnMouseLeftButtonUp( long p_xm, long p_ym );
-	virtual void		OnMouseRightButtonDown( long p_xm, long p_ym );
-	virtual void		OnMouseRightButtonUp( long p_xm, long p_ym );
-	virtual void        OnAppEvent( WPARAM p_wParam, LPARAM p_lParam );
+    virtual void OnRenderFrame( void );
+    virtual bool OnIdleAppInit( void );
+    virtual void OnAppInit( void );
+    virtual void OnClose( void );
+    virtual void OnKeyPress( long p_key ) ;
+    virtual void OnEndKeyPress( long p_key );
+    virtual void OnKeyPulse( long p_key );
+    virtual void OnMouseMove( long p_xm, long p_ym, long p_dx, long p_dy );
+    virtual void OnMouseLeftButtonDown( long p_xm, long p_ym );
+    virtual void OnMouseLeftButtonUp( long p_xm, long p_ym );
+    virtual void OnMouseRightButtonDown( long p_xm, long p_ym );
+    virtual void OnMouseRightButtonUp( long p_xm, long p_ym );
+    virtual void OnAppEvent( WPARAM p_wParam, LPARAM p_lParam );
 };
 
 #endif
