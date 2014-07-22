3879866509f4c5be9d7410b9fe44ef1d65f1e59b
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2014-07-22 20:52:25 +0200

    orbiters2 sample update

--- orbiters2/dsappclient.h
+++ orbiters2/dsappclient.h
@@ -5,169 +5,14 @@
 #include <dsapp.h>
 
 
-class MyPlanet;
-
-class Fragment
-{
-protected:
-
-    typedef DrawSpace::Core::CallBack2<Fragment, void, DrawSpace::SphericalLOD::Body*, int>     PlanetEvtCb;
-    typedef DrawSpace::Core::CallBack<Fragment, void, DrawSpace::Core::PropertyPool*>           RunnerEvtCb;
-
-
-    DrawSpace::SphericalLOD::Body*                              m_planetbody;
-    DrawSpace::Dynamics::Collider*                              m_collider;
-
-    DrawSpace::Core::Mediator::Event*                           m_buildmeshe_event;
-    DrawSpace::Core::Runner*                                    m_runner;
-    DrawSpace::Core::Task<DrawSpace::Core::Runner>*             m_task;
-
-    PlanetEvtCb*                                                m_spherelod_evt_cb;
-    RunnerEvtCb*                                                m_runner_evt_cb;
-
-    bool                                                        m_suspend_update;
-    bool                                                        m_collision_state;
-
-    DrawSpace::Utils::Mutex                                     m_meshe_ready_mutex;
-    bool                                                        m_meshe_ready;
-
-    dsreal                                                      m_planetray;
-
-    bool                                                        m_hot;
-
-
-    DrawSpace::Dynamics::CameraPoint*                           m_camera;
-    DrawSpace::Dynamics::InertBody*                             m_inertbody;
-
-    bool                                                        m_collisions;
-
-
-    dsstring                                                    m_name;
-    
-    void on_meshebuild_request( DrawSpace::Core::PropertyPool* p_args );
-    void on_spherelod_event( DrawSpace::SphericalLOD::Body* p_body, int p_currentface );
-
-    void build_meshe( DrawSpace::Core::Meshe& p_patchmeshe, int p_patch_orientation, dsreal p_sidelength, dsreal p_xpos, dsreal p_ypos, DrawSpace::Core::Meshe& p_outmeshe );
-
-public:
-
-    Fragment( const dsstring& p_name, DrawSpace::SphericalLOD::Body* p_planetbody, DrawSpace::Dynamics::Collider* p_collider, dsreal p_planetray, bool p_collisions );
-    virtual ~Fragment( void );
-
-    void Update( MyPlanet* p_owner );    
-    void SetHotState( bool p_hotstate );
-    void SetCamera( DrawSpace::Dynamics::CameraPoint* p_camera );
-    void SetInertBody( DrawSpace::Dynamics::InertBody* p_body );
-    DrawSpace::Dynamics::CameraPoint* GetCamera( void );
-    DrawSpace::Dynamics::InertBody* GetInertBody( void );
-
-    void RemoveColliderFromWorld( void );
-
-    DrawSpace::SphericalLOD::Body* GetPlanetBody( void );
-
-};
-
-
-class MyPlanet
-{
-public:
-    typedef DrawSpace::Core::CallBack2<MyPlanet, void, DrawSpace::Scenegraph::CameraEvent, DrawSpace::Core::TransformNode*>         CameraEvtCb;
-
-    typedef DrawSpace::Core::BaseCallback<void, MyPlanet*>                                                                          PlanetRelativeEventHandler;
-
-protected:
-
-
-    typedef struct
-    {
-        bool                            attached;
-        DrawSpace::Dynamics::InertBody* body;
-
-        Fragment*                       fragment;
-
-    } RegisteredBody;
-
-
-    typedef enum
-    {
-        FREE,
-        FREE_ON_PLANET,
-        INERTBODY_LINKED,
-
-    } CameraType;
-
-    typedef struct
-    {
-        bool                                update_meshe;
-
-        CameraType                          type;
-        DrawSpace::Dynamics::InertBody*     attached_body;
-
-        DrawSpace::Dynamics::CameraPoint*   camera;
-
-        Fragment*                           fragment;
-
-
-    } RegisteredCamera;
-
-    dsreal                                                      m_ray;
-
-    DrawSpace::Dynamics::World                                  m_world;
-    dsstring                                                    m_name;
-    DrawSpace::Dynamics::Orbiter*                               m_orbiter;
-        
-    DrawSpace::SphericalLOD::Drawing*                           m_drawable;
-    
-    CameraEvtCb*                                                m_camera_evt_cb;
-      
-    std::map<DrawSpace::Dynamics::InertBody*, RegisteredBody>   m_registered_bodies;
-    std::map<dsstring, RegisteredCamera>                        m_registered_camerapoints;
-    dsstring                                                    m_current_camerapoint;
-
-    std::vector<Fragment*>                                      m_planetfragments_list;
-
-    void attach_body( DrawSpace::Dynamics::InertBody* p_body );
-    void detach_body( DrawSpace::Dynamics::InertBody* p_body );
-    void body_find_attached_camera( DrawSpace::Dynamics::InertBody* p_body, std::vector<dsstring>& p_name );
-    void on_camera_event( DrawSpace::Scenegraph::CameraEvent p_event, DrawSpace::Core::TransformNode* p_node );
-
-public:
-
-    MyPlanet( const dsstring& p_name, dsreal p_ray );
-    ~MyPlanet( void );
-
-    DrawSpace::SphericalLOD::Drawing* GetDrawable( void );    
-    DrawSpace::Dynamics::Orbiter*   GetOrbiter( void );
-    DrawSpace::Dynamics::World*     GetWorld( void );
-
-    
-    CameraEvtCb* GetCameraEvtCb( void );
-
-
-    void ApplyGravity( void );
-
-    void ManageBodies( void );
-    void Update( void );
-
-    void RegisterInertBody( const dsstring& p_bodyname, DrawSpace::Dynamics::InertBody* p_body );
-    void RegisterIncludedInertBody( const dsstring& p_bodyname, DrawSpace::Dynamics::InertBody* p_body, const DrawSpace::Utils::Matrix& p_initmat );
-    bool RegisterCameraPoint( DrawSpace::Dynamics::CameraPoint* p_camera );
-
-
-    void GetName( dsstring& p_name );
-
-    void GetCameraHotpoint( const dsstring& p_name, DrawSpace::Utils::Matrix& p_outmat );
-   
-};
-
-
 class dsAppClient : public DrawSpace::App
 {
 protected:
 
     typedef DrawSpace::Core::CallBack<dsAppClient, void, DrawSpace::Gui::Widget*>   WidgetEventHandler;
 
-    typedef DrawSpace::Core::CallBack<dsAppClient, void, MyPlanet*> PlanetRelativeEvtCb;
+
+    typedef DrawSpace::Core::CallBack<dsAppClient, void, DrawSpace::Planetoid::Body*> PlanetRelativeEvtCb;
 
     typedef DrawSpace::Core::CallBack2<dsAppClient, void, DrawSpace::Scenegraph::CameraEvent, DrawSpace::Core::TransformNode*>         CameraEvtCb;
 
@@ -235,13 +80,17 @@ protected:
     DrawSpace::Dynamics::Calendar*              m_calendar;
 
 
-    MyPlanet*                                   m_planet;
+    //MyPlanet*                                   m_planet;
+
+    DrawSpace::Planetoid::Body*                 m_planet;
 
     DrawSpace::Dynamics::Orbit*                 m_orbit;
     DrawSpace::Dynamics::Centroid*              m_centroid;
 
 
-    MyPlanet*                                   m_moon;
+    //MyPlanet*                                   m_moon;
+
+    DrawSpace::Planetoid::Body*                 m_moon;
 
 
 
