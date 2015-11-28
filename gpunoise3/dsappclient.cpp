1dc91632de624093496ff27bd62c9aca1113ead3
krakoukass <manu95870@gmail.com>
2015-11-28 18:04:39 +0100

    gpunoise3 sample added

---
+++ gpunoise3/dsappclient.cpp
@@ -0,0 +1,281 @@
+#include "dsappclient.h"
+
+using namespace DrawSpace;
+using namespace DrawSpace::Interface;
+using namespace DrawSpace::Core;
+using namespace DrawSpace::Utils;
+using namespace DrawSpace::Gui;
+
+dsAppClient* dsAppClient::m_instance = NULL;
+
+
+
+dsAppClient::dsAppClient( void ) : m_mouselb( false ), m_mouserb( false )
+{    
+    _INIT_LOGGER( "loggpunoise3.conf" )  
+    m_w_title = "gpu perlin noise test 3";
+}
+
+dsAppClient::~dsAppClient( void )
+{
+
+}
+
+void dsAppClient::OnRenderFrame( void )
+{
+
+
+    DrawSpace::Interface::Renderer* renderer = DrawSpace::Core::SingletonPlugin<DrawSpace::Interface::Renderer>::GetInstance()->m_interface;
+
+    m_scenenodegraph.ComputeTransformations( m_timer );
+
+
+
+    m_texturepass->GetRenderingQueue()->Draw();
+    m_finalpass->GetRenderingQueue()->Draw();
+
+
+    renderer->DrawText( 255, 0, 0, 10, 20, "%d fps", m_timer.GetFPS() );
+
+    renderer->FlipScreen();
+
+    m_timer.Update();
+    if( m_timer.IsReady() )
+    {
+    }
+}
+
+bool dsAppClient::OnIdleAppInit( void )
+{
+    DrawSpace::Interface::Renderer* renderer = DrawSpace::Core::SingletonPlugin<DrawSpace::Interface::Renderer>::GetInstance()->m_interface;
+    renderer->SetRenderState( &DrawSpace::Core::RenderState( DrawSpace::Core::RenderState::SETCULLING, "cw" ) );
+
+
+    m_texturepass = _DRAWSPACE_NEW_( IntermediatePass, IntermediatePass( "texture_pass" ) );
+    m_texturepass->SetTargetDimsFromRenderer( false );
+    m_texturepass->SetTargetDims( 256, 256 );
+    m_texturepass->Initialize();
+    m_texturepass->CreateViewportQuad();
+    
+    m_texturepass->GetViewportQuad()->SetFx( _DRAWSPACE_NEW_( Fx, Fx ) );
+    m_texturepass->GetViewportQuad()->GetFx()->AddShader( _DRAWSPACE_NEW_( Shader, Shader( "noise3.vsh", false ) ) );
+    m_texturepass->GetViewportQuad()->GetFx()->AddShader( _DRAWSPACE_NEW_( Shader, Shader( "noise3.psh", false ) ) );
+    m_texturepass->GetViewportQuad()->GetFx()->GetShader( 0 )->LoadFromFile();
+    m_texturepass->GetViewportQuad()->GetFx()->GetShader( 1 )->LoadFromFile();
+    m_texturepass->GetViewportQuad()->GetFx()->AddRenderStateIn( DrawSpace::Core::RenderState( DrawSpace::Core::RenderState::SETTEXTUREFILTERTYPE, "none" ) );
+    m_texturepass->GetViewportQuad()->GetFx()->AddRenderStateOut( DrawSpace::Core::RenderState( DrawSpace::Core::RenderState::SETTEXTUREFILTERTYPE, "none" ) );
+
+
+    m_fractal = new Fractal( 3, 1000068, 0.5, 2.0 );
+    m_fractal2 = new Fractal( 3, 173345, 0.5, 2.0 );
+
+
+    m_perlinnoisebuffer_texture = new Texture();    
+    m_perlinnoisebuffer_texture->SetFormat( 256, 3, 4 );
+    m_perlinnoisebuffer_texture->SetPurpose( Texture::PURPOSE_FLOAT );
+
+
+    m_perlinnoisemap_texture = new Texture();
+    m_perlinnoisemap_texture->SetFormat( 256, 1, 4 );
+    m_perlinnoisemap_texture->SetPurpose( Texture::PURPOSE_FLOAT );
+
+
+
+    m_perlinnoisebuffer_texture2 = new Texture();    
+    m_perlinnoisebuffer_texture2->SetFormat( 256, 3, 4 );
+    m_perlinnoisebuffer_texture2->SetPurpose( Texture::PURPOSE_FLOAT );
+
+
+    m_perlinnoisemap_texture2 = new Texture();
+    m_perlinnoisemap_texture2->SetFormat( 256, 1, 4 );
+    m_perlinnoisemap_texture2->SetPurpose( Texture::PURPOSE_FLOAT );
+
+
+    m_texturepass->GetViewportQuad()->SetTexture( m_perlinnoisebuffer_texture, 0 );
+    m_texturepass->GetViewportQuad()->SetTexture( m_perlinnoisemap_texture, 1 );
+
+    m_texturepass->GetViewportQuad()->SetTexture( m_perlinnoisebuffer_texture2, 2 );
+    m_texturepass->GetViewportQuad()->SetTexture( m_perlinnoisemap_texture2, 3 );
+
+    
+    m_finalpass = _DRAWSPACE_NEW_( FinalPass, FinalPass( "final_pass" ) );
+    m_finalpass->Initialize();
+    m_finalpass->CreateViewportQuad();
+    m_finalpass->GetViewportQuad()->SetFx( _DRAWSPACE_NEW_( Fx, Fx ) );
+    m_finalpass->GetViewportQuad()->GetFx()->AddShader( _DRAWSPACE_NEW_( Shader, Shader( "texture.vsh", false ) ) );
+    m_finalpass->GetViewportQuad()->GetFx()->AddShader( _DRAWSPACE_NEW_( Shader, Shader( "texture.psh", false ) ) );
+    m_finalpass->GetViewportQuad()->GetFx()->GetShader( 0 )->LoadFromFile();
+    m_finalpass->GetViewportQuad()->GetFx()->GetShader( 1 )->LoadFromFile();
+    m_finalpass->GetViewportQuad()->GetFx()->AddRenderStateIn( DrawSpace::Core::RenderState( DrawSpace::Core::RenderState::SETTEXTUREFILTERTYPE, "none" ) );
+    m_finalpass->GetViewportQuad()->GetFx()->AddRenderStateOut( DrawSpace::Core::RenderState( DrawSpace::Core::RenderState::SETTEXTUREFILTERTYPE, "none" ) );
+    
+
+    m_finalpass->GetViewportQuad()->SetTexture( m_texturepass->GetTargetTexture(), 0 );
+
+
+       
+
+
+
+
+    /////////////////////////////////////////////////////////////////
+
+
+
+
+    m_finalpass->GetRenderingQueue()->UpdateOutputQueue();
+    m_texturepass->GetRenderingQueue()->UpdateOutputQueue();
+
+    m_perlinnoisebuffer_texture->AllocTextureContent();
+    m_pnbufftexture_content = m_perlinnoisebuffer_texture->GetTextureContentPtr();
+
+    m_perlinnoisemap_texture->AllocTextureContent();
+    m_pnmaptexture_content = m_perlinnoisemap_texture->GetTextureContentPtr();
+
+
+    m_perlinnoisebuffer_texture2->AllocTextureContent();
+    m_pnbufftexture_content2 = m_perlinnoisebuffer_texture2->GetTextureContentPtr();
+
+    m_perlinnoisemap_texture2->AllocTextureContent();
+    m_pnmaptexture_content2 = m_perlinnoisemap_texture2->GetTextureContentPtr();
+
+
+    float* float_ptr;
+
+    float_ptr = (float*)m_pnbufftexture_content;        
+    for(long j = 0; j < 3; j++ )
+    {
+        for( long i = 0; i < 256; i++ )    
+        {
+            float temp = m_fractal->GetNBuffer( i, j );
+            *float_ptr = temp; float_ptr++;
+        }
+    }
+
+
+    float_ptr = (float*)m_pnmaptexture_content;
+    for( long i = 0; i < 256; i++ )
+    {
+        *float_ptr = m_fractal->GetNMap( i ); float_ptr++;
+    }
+
+
+
+    float_ptr = (float*)m_pnbufftexture_content2;        
+    for(long j = 0; j < 3; j++ )
+    {
+        for( long i = 0; i < 256; i++ )    
+        {
+            float temp = m_fractal2->GetNBuffer( i, j );
+            *float_ptr = temp; float_ptr++;
+        }
+    }
+
+
+    float_ptr = (float*)m_pnmaptexture_content2;
+    for( long i = 0; i < 256; i++ )
+    {
+        *float_ptr = m_fractal2->GetNMap( i ); float_ptr++;
+    }
+
+
+
+    m_perlinnoisemap_texture->UpdateTextureContent();
+    m_perlinnoisebuffer_texture->UpdateTextureContent();
+
+    m_perlinnoisemap_texture2->UpdateTextureContent();
+    m_perlinnoisebuffer_texture2->UpdateTextureContent();
+
+
+    return true;
+}
+
+void dsAppClient::OnAppInit( void )
+{
+
+}
+
+void dsAppClient::OnClose( void )
+{
+}
+
+void dsAppClient::OnKeyPress( long p_key ) 
+{
+    switch( p_key )
+    {
+        case 'Q':
+
+            break;
+
+        case 'W':
+
+            break;
+
+        case VK_F3:
+
+            break;
+
+    }
+
+}
+
+void dsAppClient::OnEndKeyPress( long p_key )
+{
+    switch( p_key )
+    {
+        case 'Q':
+        case 'W':
+
+            break;      
+    }
+
+}
+
+void dsAppClient::OnKeyPulse( long p_key )
+{
+    switch( p_key )
+    {
+        case VK_F1:
+
+            break;
+
+        case VK_F2:
+            break;
+    }
+}
+
+void dsAppClient::OnMouseMove( long p_xm, long p_ym, long p_dx, long p_dy )
+{
+    if( m_mouserb )
+    {
+    }
+
+    else
+    {
+    }
+}
+
+void dsAppClient::OnMouseLeftButtonDown( long p_xm, long p_ym )
+{
+    m_mouselb = true;
+}
+
+void dsAppClient::OnMouseLeftButtonUp( long p_xm, long p_ym )
+{
+    m_mouselb = false;
+}
+
+void dsAppClient::OnMouseRightButtonDown( long p_xm, long p_ym )
+{
+    m_mouserb = true;
+}
+
+void dsAppClient::OnMouseRightButtonUp( long p_xm, long p_ym )
+{
+    m_mouserb = false;
+}
+
+void dsAppClient::OnAppEvent( WPARAM p_wParam, LPARAM p_lParam )
+{
+
+}
---
+++ gpunoise3/dsappclient.h
@@ -0,0 +1,77 @@
+
+#ifndef _DSAPPCLIENT_H_
+#define _DSAPPCLIENT_H_
+
+#include <dsapp.h>
+
+
+class dsAppClient : public DrawSpace::App
+{
+protected:
+
+    static dsAppClient*                                                 m_instance;
+    
+    DrawSpace::Core::SceneNodeGraph                                     m_scenenodegraph;
+
+    DrawSpace::FinalPass*                                               m_finalpass;
+
+    DrawSpace::IntermediatePass*                                        m_texturepass;
+
+
+    DrawSpace::Utils::TimeManager                                       m_timer;
+
+    bool                                                                m_mouselb;
+    bool                                                                m_mouserb;
+
+    void*                                                               m_pnbufftexture_content;
+    void*                                                               m_pnmaptexture_content;
+
+    DrawSpace::Core::Texture*                                           m_perlinnoisebuffer_texture;
+    DrawSpace::Core::Texture*                                           m_perlinnoisemap_texture;
+
+
+    void*                                                               m_pnbufftexture_content2;
+    void*                                                               m_pnmaptexture_content2;
+
+    DrawSpace::Core::Texture*                                           m_perlinnoisebuffer_texture2;
+    DrawSpace::Core::Texture*                                           m_perlinnoisemap_texture2;
+
+
+    DrawSpace::Utils::Fractal*                                          m_fractal;
+    DrawSpace::Utils::Fractal*                                          m_fractal2;
+
+
+    dsAppClient( void );
+
+public:
+
+    ~dsAppClient( void );
+
+    static dsAppClient* GetInstance( void )
+    {
+        if( m_instance == NULL )
+        {
+            m_instance = new dsAppClient;
+        }
+
+        DrawSpace::App::m_base_instance = m_instance;
+        return m_instance;
+    }
+
+    virtual void OnRenderFrame( void );
+    virtual bool OnIdleAppInit( void );
+    virtual void OnAppInit( void );
+    virtual void OnClose( void );
+    virtual void OnKeyPress( long p_key ) ;
+    virtual void OnEndKeyPress( long p_key );
+    virtual void OnKeyPulse( long p_key );
+    virtual void OnMouseMove( long p_xm, long p_ym, long p_dx, long p_dy );
+    virtual void OnMouseLeftButtonDown( long p_xm, long p_ym );
+    virtual void OnMouseLeftButtonUp( long p_xm, long p_ym );
+    virtual void OnMouseRightButtonDown( long p_xm, long p_ym );
+    virtual void OnMouseRightButtonUp( long p_xm, long p_ym );
+    virtual void OnAppEvent( WPARAM p_wParam, LPARAM p_lParam );
+};
+
+#endif
+
