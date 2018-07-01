a46119699902485d8994a4a3e3507eb21d2ee9b6
NevilClavain <emmanuel.chaumont@wanadoo.f>
2018-07-01 21:02:34 +0200

    updated

--- bin/test_data/scripts_bank/test2_i.lua
+++ bin/test_data/scripts_bank/test2_i.lua
@@ -8,7 +8,7 @@ lights =
 	light0 = 
 	{
 		color = { r = 1.0, g = 1.0, b = 1.0, a = 1.0 },
-		direction = { x = 1.0, y = -1.0, z = -6.0, w = 1.0 },
+		direction = { x = 0.0, y = -1.0, z = 0.0, w = 1.0 },
 	}
 }
 
