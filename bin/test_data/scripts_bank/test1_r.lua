76a051932be66959213524c4f9a33171b432d221
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2019-07-20 10:22:45 +0200

    Update test1_r.lua

--- bin/test_data/scripts_bank/test1_r.lua
+++ bin/test_data/scripts_bank/test1_r.lua
@@ -8,6 +8,5 @@ g:remove_appruncb('run')
 rg:release_pass_viewportquad_resources('final_pass')
 rg:remove_pass_viewportquad('final_pass')
 
-
+rg:update_renderingqueues()
 g:release_assets()
---rg:update_renderingqueues()
