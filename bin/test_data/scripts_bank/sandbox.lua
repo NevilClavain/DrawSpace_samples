669020f37dc70167b262209e1f443568ffa75312
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2019-12-17 19:02:15 +0100

    sandbox : functions to explore model file data

--- bin/test_data/scripts_bank/sandbox.lua
+++ bin/test_data/scripts_bank/sandbox.lua
@@ -23,50 +23,69 @@ end)
 g:signal_renderscenebegin("eg")
 
 
-inspect_model = function(entity_name, model_file_path)
+model = {}
+model.anims = {}
+model.meshes = {}
+
+model.load = function(entity_name, model_file_path)
 
   local meshe_entity=Entity()
 
   meshe_entity:add_aspect(RESOURCES_ASPECT)
   meshe_entity:configure_mesheresource(model_file_path)
-
   
   eg:add_child('root', entity_name, meshe_entity)
   return meshe_entity
 end
 
-h = function()
-  e = inspect_model('e','Trex.fbx')
+model.unload = function(meshe_entity)
+
+  local entity_name = eg:find_entityname(meshe_entity)
+  meshe_entity:release_mesheresource()
+  meshe_entity:remove_aspect(RESOURCES_ASPECT)
+
+  eg:remove(entity_name)
 end
 
-h2 = function()
-  local model_root_infos = { e:read_meshesfiledescription('Trex.fbx', 'root') }
-  g:print( 'model file = '..model_root_infos[1]..' num_meshes = '..model_root_infos[3]..' num_animations = '..model_root_infos[5] )
+l = function()
+  e = model.load('e','Trex.fbx')
 end
 
-h3 = function()
-  local meshes_id_list = { e:read_meshesfiledescription('Trex.fbx', 'meshes_list') }
-  for k, v in pairs(meshes_id_list) do
-    g:print(k.. ' - ' ..v)
+u = function()
+  model.unload(e)
+  e = nil
+end
+
+model.dump = function(entity)
+  local list_size = entity:read_meshesfiledescriptionssize()   	
+  for i = 1, list_size do
+    local model_root_infos = { entity:read_meshesfiledescription(i, 'root') }
+	g:print( i..' - model file = '..model_root_infos[1]..' num_meshes = '..model_root_infos[3]..' num_animations = '..model_root_infos[5] )
   end
 end
 
-h4 = function()
-  local anims_id_list = { e:read_meshesfiledescription('Trex.fbx', 'anims_list') }
-  for k, v in pairs(anims_id_list) do
-    g:print(k.. ' - ' ..v)
-  end
+model.mesheslist = function(entity, index)
+    local meshes_id_list = { e:read_meshesfiledescription(index, 'meshes_list') }
+	for k, v in pairs(meshes_id_list) do
+	  g:print(k.. ' - ' ..v)
+	end
 end
 
-h5 = function(anim_index)
-  local anim_infos = { e:read_meshesfiledescription('Trex.fbx', 'anim', anim_index) }
-   g:print( 'name = '..anim_infos[1]..' ticks/sec = '..anim_infos[2]..' duration = '..anim_infos[3]..'s number of channels = '..anim_infos[4])
+model.animslist = function(entity, index)
+    local anims_id_list = { e:read_meshesfiledescription(index, 'anims_list') }
+	for k, v in pairs(anims_id_list) do
+	  g:print(k.. ' - ' ..v)
+	end
 end
 
-h6 = function(meshe_index)
-  local meshe_infos = { e:read_meshesfiledescription('Trex.fbx', 'meshe', meshe_index) }
-   g:print('node id = '..meshe_infos[1].. ' name = ['..meshe_infos[2]..']' )
-   g:print('has positions = '..meshe_infos[3]..' has faces = '..meshe_infos[4]..' has normales = '..meshe_infos[5]..' has tbn = '..meshe_infos[6] )
-   g:print('num vertices = '..meshe_infos[7]..' num faces = '..meshe_infos[8]..' num uvchannels = '..meshe_infos[9]..' num bones = '..meshe_infos[10])
+model.anims.dump = function(entity, index, anim_index)
+  local anim_infos = { e:read_meshesfiledescription(index, 'anim', anim_index) }
+  g:print('name = '..anim_infos[1]..' ticks/sec = '..anim_infos[2]..' duration = '..anim_infos[3]..'s number of channels = '..anim_infos[4])
+end
 
+model.meshes.dump = function(entity, index, meshe_index)
+  local meshe_infos = { e:read_meshesfiledescription(index, 'meshe', meshe_index) }
+  g:print('node id = '..meshe_infos[1].. ' name = ['..meshe_infos[2]..']' )
+  g:print('has positions = '..meshe_infos[3]..' has faces = '..meshe_infos[4]..' has normales = '..meshe_infos[5]..' has tbn = '..meshe_infos[6] )
+  g:print('num vertices = '..meshe_infos[7]..' num faces = '..meshe_infos[8]..' num uvchannels = '..meshe_infos[9]..' num bones = '..meshe_infos[10])  
 end
