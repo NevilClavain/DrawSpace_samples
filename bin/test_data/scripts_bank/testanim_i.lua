638909f3c5a3bf0a8fd0e3635aeb75e732fb759f
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2019-12-22 10:57:52 +0100

    Update testanim_i.lua

--- bin/test_data/scripts_bank/testanim_i.lua
+++ bin/test_data/scripts_bank/testanim_i.lua
@@ -245,107 +245,6 @@ dino_transform:add_matrix( "pos", dino_pos_mat )
 
 
 
-neck_entity_config = 
-{ 
-	texture_pass = 
-	{
-		fx = 
-		{
-			shaders = 
-			{
-				{ path='lit.vso',mode=SHADER_COMPILED },
-				{ path='lit.pso',mode=SHADER_COMPILED }
-
-			},
-			rs_in = 
-			{
-				{ ope=RENDERSTATE_OPE_ENABLEZBUFFER, value="true" },
-				{ ope=RENDERSTATE_OPE_SETCULLING, value="none" },
-			},
-			rs_out =
-			{
-				{ ope=RENDERSTATE_OPE_ENABLEZBUFFER, value="false" },
-				{ ope=RENDERSTATE_OPE_SETCULLING, value="cw" },
-			}
-		},
-		textures =
-		{
-			[1] = 
-			{			
-			}
-		},
-		vertex_textures =
-		{
-		},
-		rendering_order = 10000,
-		shaders_params = commons.setup_lit_shader_params()
-	},
-
-	meshes_loader_params =
-	{
-		normale_generation_mode = NORMALES_AUTO_SMOOTH,
-		tb_generation_mode = TB_DISCARDED,
-	},
-}
-
-neck_entity, neck_renderer = commons.create_rendered_meshe(neck_entity_config, 'head.ac', 'object')
-
---declare bone animation for neck
---neck_renderer:set_shaderrealinvector( 'texture_pass', 'flags_v', 2, 1.0)
---neck_entity:add_aspect(ANIMATION_ASPECT)
-
-neck_renderer:register_to_rendering(rg)
-eg:add_child('root','neck_entity',neck_entity)
-
-neck_material =
-{
-	specular_power = 999.0,
-	color_source = { r = 0.0, g = 0.0, b = 0.0, a = 0.0 },
-	simple_color = { r = 1.0, g = 1.0, b = 1.0, a = 1.0 },
-	light_absorption = { r = 0.0, g = 0.0, b = 0.0, a = 0.0 },
-	self_emissive = { r = 0.0, g = 0.0, b = 0.0, a = 1.0 },
-	--bump_mapping = { texture_size = 1024, bias = 0.193 }
-}
-
-commons.apply_material( neck_material, neck_renderer, 'texture_pass')
-
-
-renderers[nb_renderers] = neck_renderer
-nb_renderers = nb_renderers + 1
-
-
-neck_pos_mat = Matrix()
-neck_pos_mat:translation( 0.0, 2.0, 0.0 )
-
-neck_rot1_mat = Matrix()
-neck_rot2_mat = Matrix()
-neck_rotlerp_mat = Matrix()
-
-neck_rot1_quat = Quaternion()
-neck_rot2_quat = Quaternion()
-
-neck_rotlerp_quat = Quaternion()
-
-
-neck_rot1_quat:rotation_axis(1.0, 0.0, 0.0, commons.utils.deg_to_rad( 127.0 ))
-neck_rot1_mat:rotation_fromquaternion(neck_rot1_quat)
-
-neck_rot2_quat:rotation_axis(0.0, 1.0, 1.0, commons.utils.deg_to_rad( 102.0 ))
-neck_rot2_mat:rotation_fromquaternion(neck_rot2_quat)
-
-neck_rotlerp_quat:store_lerp(neck_rot1_quat,neck_rot2_quat, 0.0)
-
-neck_rotlerp_mat:rotation_fromquaternion(neck_rotlerp_quat)
-
-neck_transform = RawTransform()
-neck_transform:configure(neck_entity)
---neck_transform:add_matrix( "rot1", neck_rot1_mat )
---neck_transform:add_matrix( "rot2", neck_rot2_mat )
-neck_transform:add_matrix( "rotlerp", neck_rotlerp_mat	 )
-neck_transform:add_matrix( "pos", neck_pos_mat )
-
-
-
 commons.update_lights( 'texture_pass', lights, renderers )
 
 commons.setup_lit_flags_simple( 'texture_pass', renderers, fog_intensity, fog_color)
@@ -359,33 +258,19 @@ eg:set_camera(camera_entity)
 rg:update_renderingqueues()
 
 
-cube_rot_y=SyncAngle()
-cube_rot_x=SyncAngle()
 
-
-
-
-cube_rot_x:init_fromtimeaspectof(root_entity,0.0)
-cube_rot_y:init_fromtimeaspectof(root_entity,0.0)
 
 
 
 g:add_mousemovecb( "onmousemove",
 function( xm, ym, dx, dy )  
 
-  if ctrl_key == TRUE then
-    
-    cube_rot_y:inc(20.0 * dx)
-	cube_rot_x:inc(20.0 * dy)
+  local mvt_info = { camera_mvt:read() }
 
+  if mouse_right == FALSE then
+	camera_mvt:update(mvt_info[4],mvt_info[1],mvt_info[2],mvt_info[3],-dy / 4.0,-dx / 4.0, 0)
   else
-      local mvt_info = { camera_mvt:read() }
-
-	if mouse_right == FALSE then
-	  camera_mvt:update(mvt_info[4],mvt_info[1],mvt_info[2],mvt_info[3],-dy / 4.0,-dx / 4.0, 0)
-	else
-	  camera_mvt:update(mvt_info[4],mvt_info[1],mvt_info[2],mvt_info[3],0,0,-dx)
-	end
+	camera_mvt:update(mvt_info[4],mvt_info[1],mvt_info[2],mvt_info[3],0,0,-dx)
   end
 
 end)
