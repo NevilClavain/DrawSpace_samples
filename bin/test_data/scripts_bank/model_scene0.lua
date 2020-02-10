64011175e025901082f26f9bfff26a361330e0f6
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2020-02-10 14:40:17 +0100

    spherebump model added

--- bin/test_data/scripts_bank/model_scene0.lua
+++ bin/test_data/scripts_bank/model_scene0.lua
@@ -3,15 +3,19 @@
 	g:do_file('continent_model.lua')
 
 	
-	g:do_file('raptor_model.lua')
-	g:do_file('trex_model.lua')
-	g:do_file('metalcube_model.lua')
+	include('raptor_model.lua')
+	include('trex_model.lua')
+	include('metalcube_model.lua')
+    include('spherebump_model.lua')
 	
 	continent.view.load('continent')
 	
 	raptor.view.load('r')
 	trex.view.load('t')
 	metalcube.view.load('c', {x = -4010.0, y = 15.0, z = -2740.0} )
+	spherebump.view.load('s', {x = -4010.0, y = 135.0, z = -2740.0} )
+
+	spherebump.view.load('s2', {x = -4010.0, y = 0, z = -2740.0} )
 
     model.move.setpos('r', -4020.0, 0, -2740)
 	model.move.setpos('t', -4068.0, 0, -2740)
