c520670d26b6e8af515d111272abade992da032d
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2020-03-04 21:41:49 +0100

    model.dump.show ouptut now in infos widget

--- bin/test_data/scripts_bank/model_dump.lua
+++ bin/test_data/scripts_bank/model_dump.lua
@@ -24,30 +24,34 @@ end
 
 model.dump.show = function(entity)
 
+  model.text = ""
   local list_size = entity:read_meshesfiledescriptionssize()   	
   for i = 1, list_size do
     local model_root_infos = { entity:read_meshesfiledescription(i, 'root') }
-	g:print( i..' - model file = '..model_root_infos[1]..' num_meshes = '..model_root_infos[3]..' num_animations = '..model_root_infos[5] )
+	model.text = model.text..i..' - model file = '..model_root_infos[1]..' num_meshes = '..model_root_infos[3]..' num_animations = '..model_root_infos[5].."\n"
 	
-	
-	g:print('Meshes : ')
+	model.text = model.text..'Meshes : \n'
+
     local meshes_id_list = { entity:read_meshesfiledescription(i, 'meshes_list') }
 	for k, v in pairs(meshes_id_list) do
-	  g:print(k.. ' - ' ..v)
+	  model.text = model.text..k.. ' - ' ..v.."\n"
 
       local meshe_infos = { entity:read_meshesfiledescription(i, 'meshe', k) }
-      g:print('node id = '..meshe_infos[1].. ' name = ['..meshe_infos[2]..']' )
-      g:print('has positions = '..meshe_infos[3]..' has faces = '..meshe_infos[4]..' has normales = '..meshe_infos[5]..' has tbn = '..meshe_infos[6] )
-      g:print('num vertices = '..meshe_infos[7]..' num faces = '..meshe_infos[8]..' num uvchannels = '..meshe_infos[9]..' num bones = '..meshe_infos[10])  
-
+	  model.text = model.text..'node id = '..meshe_infos[1].. ' name = ['..meshe_infos[2]..']'.."\n"	 
+	  model.text = model.text..'has positions = '..meshe_infos[3]..' has faces = '..meshe_infos[4]..' has normales = '..meshe_infos[5]..' has tbn = '..meshe_infos[6].."\n"
+	  model.text = model.text..'num vertices = '..meshe_infos[7]..' num faces = '..meshe_infos[8]..' num uvchannels = '..meshe_infos[9]..' num bones = '..meshe_infos[10].."\n"
 	end
 
-	g:print('Animations : ')
+	model.text = model.text..'Animations : \n'
     local anims_id_list = { entity:read_meshesfiledescription(i, 'anims_list') }
 	for k, v in pairs(anims_id_list) do	 
       local anim_infos = { entity:read_meshesfiledescription(i, 'anim', k) }
-      g:print(k.. ' - ' ..v..' name = '..anim_infos[1]..' ticks/sec = '..anim_infos[2]..' duration = '..anim_infos[3]..'s number of channels = '..anim_infos[4])
+	  model.text = model.text..k.. ' - ' ..v..' name = '..anim_infos[1]..' ticks/sec = '..anim_infos[2]..' duration = '..anim_infos[3]..'s number of channels = '..anim_infos[4].."\n"
 	end
+  end
+
+  if model.dump.showcontentfunc ~= nil then
+    model.dump.showcontentfunc()
   end
 
 end
